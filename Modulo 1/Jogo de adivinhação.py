58560fdc00605a524f5cc5c699ae6df1039bfbfc
Aragao05 <130008592+Aragao05@users.noreply.github.com>
2023-10-26 14:17:30 -0400

    troca de laço de repetição
    mudei o tipo de laço de while para for, e adicionei uma linha pra informar a rodada atual

--- Modulo 1/Jogo de adivinhação.py
+++ Modulo 1/Jogo de adivinhação.py
@@ -5,23 +5,29 @@ print("Bem vindo ao jogo de adivinhação")
 print("################################")
 
 numero_secreto = 42
-tentativas = 1
+tentativas = 3
 
-while (tentativas < 4):
-    print("Tentativa numero {} de 3".format(tentativas))
+for i in range(0,tentativas):
+    rodada = i + 1
+    print("Tentativa numero {} de 3".format(rodada))
+    print("Digite um numero entre 1 e 100")
     chute = int(input("Digite o numero:"))
 
     acertou = chute == numero_secreto
     menor = chute > numero_secreto
     maior = chute < numero_secreto
+
+    if (chute < 1 or chute > 100):
+        print("Voce deve digitar um numero entre 1 e 100")
+        continue
     
     if (acertou):
         print("Parebens voce acertou o numero sorteado")
         break
     else:
         if (menor):
-            print("Voce errou, o numero é menor")
+            print("\nVoce errou, o numero é menor\n")
         elif(maior):
-            print("Voce errou, o numero é maior")
-
-    tentativas += 1
+            print("\nVoce errou, o numero é maior\n")
+    
+print("Fim do jogo")
---
+++ Testes.py
@@ -0,0 +1,3 @@
+fila = ("beta",2,"jota",4,5)
+for i in fila:
+    print(i)
