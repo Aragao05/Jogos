2073f5e20dfe0a05cef3e0947e1c61baadb84a10
Aragao05 <130008592+Aragao05@users.noreply.github.com>
2023-11-21 15:45:20 -0400

    Atributos privados
    Aprendi como fazer um atributo privado, e aprendi metodos getters e setters e proriedades

--- Modulo 3 OO/Conta.py
+++ Modulo 3 OO/Conta.py
@@ -1,21 +1,26 @@
 class Conta:
 
     #Atributos
+    #se o atributo tem "__"no nome ele é privado 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     #Metodos
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
     
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
 conta = Conta(123, "Vini", 55.0, 1000.0)
 conta2 = Conta(321, "Claudio", 100.50, 3500.0)
